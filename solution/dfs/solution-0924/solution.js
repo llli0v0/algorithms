878740ac80b98b599fd270d7f33a7e19afd42c4a
llli0v0 <li.gang.web@gmail.com>
2019-03-07 23:02:16 +0800

    solution 924

---
+++ solution/dfs/solution-0924/solution.js
@@ -0,0 +1,81 @@
+/**
+ * @param {number[][]} graph
+ * @param {number[]} initial
+ * @return {number}
+ */
+var minMalwareSpread = function(graph, initial) {
+  initial = initial.sort((a, b) => a - b);
+  let initialObj = {}, currentMinSpread = Infinity, result, V = {};
+  let currentSpread = initial.length - 1;
+  build();
+  if (!Object.keys(V).length) return initial[0];
+  findBest();
+  return result;
+  function build() {
+    for (let i = 0; i < initial.length; i++) initialObj[initial[i]] = true;
+    for (let i = 0; i < graph.length; i++) {
+      for (let j = 0; j < graph.length; j++) {
+        if (graph[i][j] && i !== j) {
+          if (V[i] === undefined) {
+            V[i] = { state: 'B', neighbor: new Set() };
+            if (initialObj[i]) V[i].state = 'R';
+          }
+          if (V[j] === undefined) {
+            V[j] = { state: 'B', neighbor: new Set() };
+            if (initialObj[j]) V[j].state = 'R';
+          }
+          V[i].neighbor.add(j);
+          V[j].neighbor.add(i);
+        }
+      }
+    }
+  }
+  function findBest() {
+    for (let i = 0; i < initial.length; i++) {
+      V[initial[i]] && (V[initial[i]].state = 'B');
+      for (let m = 0; m < i; m++) {
+        if (V[initial[m]]) spread(initial[m]);
+      }
+      for (let n = i + 1; n < initial.length; n++) {
+        if (V[initial[n]]) spread(initial[n]);
+      }
+      V[initial[i]] && (V[initial[i]].state = 'R');
+      if (currentSpread < currentMinSpread) {
+        currentMinSpread = currentSpread;
+        result = initial[i];
+      }
+      clean();
+    }
+  }
+  function spread(v) {
+    let stack = [...V[v].neighbor];
+    while (stack.length) {
+      let key = stack.pop();
+      let current = V[key];
+      if (current.state === 'B') {
+        currentSpread++;
+        stack = stack.concat([...current.neighbor]);
+        current.state = 'R';
+      }
+    }
+  }
+  function clean() {
+    for (let key in V) {
+      if (!initialObj[key]) V[key].state = 'B';
+    }
+    currentSpread = initial.length - 1;
+  }
+};
+
+console.log(minMalwareSpread([
+  [1,0,0,0,1,0,0,0,0,0],
+  [0,1,1,0,0,0,0,0,0,0],
+  [0,1,1,0,0,1,0,0,0,0],
+  [0,0,0,1,0,0,0,0,0,0],
+  [1,0,0,0,1,0,0,0,0,0],
+  [0,0,1,0,0,1,0,0,0,0],
+  [0,0,0,0,0,0,1,0,0,1],
+  [0,0,0,0,0,0,0,1,0,0],
+  [0,0,0,0,0,0,0,0,1,0],
+  [0,0,0,0,0,0,1,0,0,1]],
+  [1,3,0]));
