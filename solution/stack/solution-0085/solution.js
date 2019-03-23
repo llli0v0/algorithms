c964f6cf4ecad7bfaac13bf7921dddfc592a6b67
llli0v0 <li.gang.web@gmail.com>
2019-03-23 11:09:48 +0800

    redo 85

--- solution/stack/solution-0085/solution.js
+++ solution/stack/solution-0085/solution.js
@@ -3,34 +3,7 @@
  * @return {number}
  */
 var maximalRectangle = function(matrix) {
-  if (matrix.length === 0) return 0;
-  var largestRectangleArea = function(heights) {
-    let stack = [];
-    let currentMax = 0;
-    for (let i = 0; i < heights.length; i++) {
-      if (stack.length === 0) {
-        stack.push(i);
-        continue;
-      }
-      while (stack.length && heights[stack[stack.length - 1]] > heights[i]) {
-        let top = stack.pop();
-        if (stack.length === 0) {
-          currentMax = Math.max(i * heights[top], currentMax);
-        } else {
-          currentMax = Math.max((i - stack[stack.length - 1] - 1) * heights[top], currentMax);
-        }
-      }
-      stack.push(i);
-    }
-    for (let i = 0; i < stack.length; i++) {
-      if (i === 0) {
-        currentMax = Math.max(heights[stack[0]] * heights.length, currentMax);
-      } else {
-        currentMax = Math.max(heights[stack[i]] * (heights.length - stack[i - 1] - 1), currentMax);
-      }
-    }
-    return currentMax;
-  };
+  if (!matrix.length) return 0;
   let currentHistogram = Array(matrix[0].length).fill(0);
   let result = 0;
   for (let i = 0; i < matrix.length; i++) {
@@ -44,4 +17,20 @@ var maximalRectangle = function(matrix) {
     result = Math.max(result, largestRectangleArea(currentHistogram));
   }
   return result;
+  function largestRectangleArea(heights) {
+    if (!heights.length) return 0;
+    let stack = [];
+    let result = 0;
+    for (let i = 0; i < heights.length; i++) {
+      while (stack.length && heights[stack[stack.length - 1]] > heights[i]) {
+        result = Math.max(result, heights[stack.pop()] * (stack.length ? i - stack[stack.length - 1] - 1 : i));
+      }
+      stack.push(i);
+    }
+    result = Math.max(heights[stack[0]] * heights.length, result);
+    for (let i = 1; i < stack.length; i++) {
+      result = Math.max(heights[stack[i]] * (heights.length - stack[i - 1] - 1), result);
+    }
+    return result;
+  };
 };
