7491b76262960917c6bb56de0b463b964fa9a499
llli0v0 <li.gang.web@gmail.com>
2019-03-31 17:18:20 +0800

    solution 78

---
+++ solution/backtracking/solution-0078/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+  let re = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    let d = '' + nums[i];
+    findSub(d, nums.slice(i + 1));
+    re.add(d);
+  }
+  return Array.from(re).map(i => i.split('_')).concat([[]]);
+  function findSub(s, ar) {
+    if (!ar.length) return;
+    for (let i = 0; i < ar.length; i++) {
+      let ss = s + '_' + ar[i];
+      findSub(ss, ar.slice(i + 1));
+      re.add(ss);
+    }
+  }
+};
