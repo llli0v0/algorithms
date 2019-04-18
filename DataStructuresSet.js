3f6eb273c080d5f21040ee5aaa13b0c43d709746
llli0v0 <li.gang.web@gmail.com>
2019-04-18 16:03:19 +0800

    del tree

--- DataStructuresSet.js
+++
@@ -1,9 +0,0 @@
-const BST = require('./data-structures/tree/binary-search-tree/BinarySearchTree');
-const BSTNode = require('./data-structures/tree/binary-search-tree/BinarySearchTreeNode');
-const { MinHeap } = require('./data-structures/heap/heap');
-
-module.exports = {
-  BST,
-  BSTNode,
-  MinHeap
-};
--- data-structures/tree/binary-search-tree/BinarySearchTreeNode.js
+++
@@ -1,9 +0,0 @@
-class BinarySearchTreeNode {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
-}
-
-module.exports = BinarySearchTreeNode;
