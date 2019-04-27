aa999a32b97ed3b9613f9894cc8c5e99d9d5e0fc
llli0v0 <li.gang.web@gmail.com>
2019-04-27 18:22:27 +0800

    Get into the habit of adding notes (Chinese and English)

--- leetcode-solutions/math/solution-0902/solution.js
+++ leetcode-solutions/math/solution-0902/solution.js
@@ -7,11 +7,13 @@ var atMostNGivenDigitSet = function(D, N) {
   let result = 0;
   N = String(N);
   // Result + the number of digits that are less than N
+  // 首先统计所有位数小于N的数量
   for (let i = 1; i < N.length; i++) {
     result += Math.pow(D.length, i);
   }
   // If the number of elements result+ that are equal to N[I] in D is less than N.slice (I),
   // note that 0 does not exist in D
+  // 然后如果D中存在和N[i]相等的元素统计，位数和N.slice(i)相等！(因为D中不存在0)并且小于等于N.slice(i)的数量
   for (let i = 0; i < N.length; i++) {
     let c = lessCount(N[i], i === N.length - 1 ? true : false);
     result += c * Math.pow(D.length, N.length - i - 1);
