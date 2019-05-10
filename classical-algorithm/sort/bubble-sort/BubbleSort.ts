e5b7aeacf40406425afd9e7809351e5c2742460d
llli0v0 <li.gang.web@gmail.com>
2019-05-10 16:27:37 +0800

    bubble sort better

--- classical-algorithm/sort/bubble-sort/BubbleSort.ts
+++ classical-algorithm/sort/bubble-sort/BubbleSort.ts
@@ -1,10 +1,10 @@
 function BubbleSort(A: number[]): number[] {
-  let times = A.length;
-  while (times - 1) {
-    for (let i: number = 0; i < A.length - 1; i++) {
+  let needSort: number = A.length;
+  while (needSort - 1) {
+    for (let i: number = 0; i < needSort - 1; i++) {
       if (A[i] > A[i + 1]) [A[i], A[i + 1]] = [A[i + 1], A[i]];
     }
-    times--;
+    needSort--;
   }
   return A;
 }
