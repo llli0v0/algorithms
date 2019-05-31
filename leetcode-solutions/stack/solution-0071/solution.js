aced383b8058606da4980eb4938ae1e9abbb66c4
llli0v0 <li.gang.web@gmail.com>
2019-05-31 22:26:06 +0800

    solution 71

---
+++ leetcode-solutions/stack/solution-0071/solution.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function(path) {
+  let stack = [];
+  path += '/';
+  for (let i = 0; i < path.length; i++) {
+    if (path[i] === '/') {
+      if (stack[stack.length - 1] === '..') {
+        if (stack.length > 1) stack.splice(stack.length - 3);
+      } else if (stack[stack.length - 1] !== '/') {
+        if (stack[stack.length - 1] === '.') {
+          stack.pop();
+          if (stack.length < 2) continue;
+          stack.pop();
+        }
+        stack.push(path[i]);
+      }
+    } else {
+      if (stack[stack.length - 1] === '/') {
+        stack.push(path[i]);
+      } else {
+        stack[stack.length - 1] += path[i];
+      }
+    }
+  }
+  if (stack[stack.length - 1] === '/' && stack.length > 1) stack.pop();
+  return stack.join('');
+};
