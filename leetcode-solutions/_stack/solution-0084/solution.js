22cfc06bf95e6ad060672b53d7deb84c9878b850
llli0v0 <li.gang.web@gmail.com>
2019-06-06 13:15:07 +0800

    回顾 solution 84

--- leetcode-solutions/_stack/solution-0084/solution.js
+++ leetcode-solutions/_stack/solution-0084/solution.js
@@ -2,6 +2,21 @@
  * @param {number[]} heights
  * @return {number}
  */
+
+/**
+ * 
+ * 回顾
+ * 
+ * 问题的核心是对于 heights[i] 寻找 heigths[i] 的左右边界
+ * 当把 heigths[i] 插入到 stack 中时
+ * 我们利用 stack 来记录 heights 项的 index
+ * 如果 heigths[stack[stack.length - 1]] < heigths[i]
+ * 我们就找到了第 stack[stack.length - 1] 项的 右边界
+ * 同时 while 循环 直到 stack.length === 0 || heigths[stack[stack.length - 1]] < heights[i]
+ * 这时 我们找到了 heights[i] 的左边界
+ * heights[i] 一直在 stack 中 停留 直到 heigths[i] 被 pop
+ * result = Math.max(result, heights[i] * (i - stack[stack.length - 1] - 1))
+ */
 var largestRectangleArea = function(heights) {
   if (!heights.length) return 0;
   let stack = [];
---
+++ leetcode-solutions/_stack/solution-0084/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def largestRectangleArea(self, heights):
+        if not heights:
+            return 0
+        stack, result = [0], heights[0]
+        for i in range(1, len(heights)):
+            while stack and heights[i] < heights[stack[-1]]:
+                result = max(result, heights[stack.pop()] * (i - stack[-1] - 1 if stack else i))
+            stack.append(i)
+        result = max(result, heights[stack[0]] * len(heights))
+        for i in range(1, len(stack)):
+            result = max(result, heights[stack[i]] * (len(heights) - stack[i - 1] - 1))
+        return result
+
+if __name__ == '__main__':
+    S = Solution()
+    print(S.largestRectangleArea([2,1,2]))
