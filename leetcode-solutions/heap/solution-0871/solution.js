dc2fb945307f80213c97d93633ff02be94a85504
llli0v0 <li.gang.web@gmail.com>
2019-06-13 19:56:15 +0800

    solution 871

---
+++ leetcode-solutions/heap/solution-0871/solution.js
@@ -0,0 +1,22 @@
+const Heap = require('../../../data-structures/heap/heap');
+
+/**
+ * @param {number} target
+ * @param {number} startFuel
+ * @param {number[][]} stations
+ * @return {number}
+ */
+var minRefuelStops = function(target, startFuel, stations) {
+  let heap = new Heap();
+  let result = 0;
+  while (stations.length) {
+    while (stations.length && stations[0][0] <= startFuel) heap.heappush(-stations.shift()[1]);
+    while (stations.length && startFuel < stations[0][0] || !stations.length && startFuel < target) {
+      if (!heap.heap.length) return -1;
+      startFuel -= heap.heappop();
+      result++;
+    }
+  }
+  if (startFuel >= target) return result;
+  return -1;
+};
