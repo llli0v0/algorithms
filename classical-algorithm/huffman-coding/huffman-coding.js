444bacb921fda93098fd3b4f5de4d292eb1fa990
llli0v0 <li.gang.web@gmail.com>
2019-06-14 17:23:45 +0800

    回顾哈夫曼编码

--- classical-algorithm/huffman-coding/huffman-coding.js
+++ classical-algorithm/huffman-coding/huffman-coding.js
@@ -1,5 +1,33 @@
 const Heap = require('../../data-structures/heap/heap');
 
+/**
+ * 
+ * 回顾
+ * 
+ * huffman 的核心是用二叉树无二意编码(即最终得到的每个字符的编码不是任何其他字符编码的前缀)
+ * 构建最优二叉树
+ * 
+ * 最优二叉树
+ * 假设 现在要压缩 'hello world'
+ * 首先统计字符频率
+ * { h: 1, e: 1, l: 3, o: 2, ' ': 1, w: 1, r: 1, d: 1 }
+ * 在 堆 中
+ * heap = [ TreeNode{ key: 'h', val: 1 }, TreeNode{ key: 'e', val: 1 }, TreeNode{ key: 'w', val: 1 }, TreeNode{ key: 'd', val: 1 },
+ *   TreeNode{ key: ' ', val: 1 }, TreeNode{ key: 'l', val: 3 }, TreeNode{ key: 'r', val: 1 }, TreeNode{ key: 'o', val: 1 } ]
+ * 然后 heappop 两个节点 TreeNode{ key: 'h', val: 1 } TreeNode{ key: 'w', val: 1 }
+ * 将这两个节点合并为一个节点 并重新翻入 堆 中
+ * 这时候的 堆
+ * heap = [ TreeNode{ key: 'r', val: 1 }, TreeNode{ key: 'e', val: 1 }, TreeNode{ key: 'o', val: 2 }, TreeNode{ key: 'd', val: 1 }
+ *   TreeNode{ key: ' ', val: 1 }, TreeNode{ key: 'l', val: 3 }, TreeNode{ val: 2 } ]
+ * 以此类推......
+ * 直到 堆 中只有一个 TreeNode
+ * 仅剩的这个节点就是最优二叉树的root
+ * 
+ * 其实最优二叉树的原理很好理解
+ * 每次从 堆 中拿两个val最小TreeNode
+ * 如果把这两个节点的频率和 看做是一个字符的频率呢？ 这样想就简单多了
+ */
+
 class TreeNode {
   constructor(val) {
     this.val = val;
@@ -26,7 +54,6 @@ class HuffmanTree {
       node.key = key;
       heap.heappush(node);
     }
-    this.root = heap[0];
     while (heap.heap.length > 1) {
       let l = heap.heappop();
       let r = heap.heappop();
@@ -40,25 +67,21 @@ class HuffmanTree {
 
 class HuffmanEncoding {
   constructor() {
-    this.codeDict = {};
+    this.dict = {};
   }
 
   getHuffmanCoding(str) {
     let huffmanTree = new HuffmanTree(str);
-    this.codeDict = huffmanTree.counter;
+    this.dict = huffmanTree.counter;
     this.getCoding(huffmanTree.root, '');
-    let codeStr = '';
-    for (let i = 0; i < str.length; i++) {
-      codeStr += this.codeDict[str[i]];
-    }
-    return codeStr;
+    let s = '';
+    for (let i = 0; i < str.length; i++) s += this.dict[str[i]];
+    return s;
   }
 
   getCoding(node, s) {
-    if (!node.left && !node.right) this.codeDict[node.key] = s || '1';
+    if (!node.left && !node.right) this.dict[node.key] = s || '1';
     if (node.left) this.getCoding(node.left, s + '0');
     if (node.right) this.getCoding(node.right, s + '1');
   }
 }
-
-module.exports = HuffmanEncoding;
