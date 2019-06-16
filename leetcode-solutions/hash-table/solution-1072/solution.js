b85c48b86f4a2095831ae0ad00d974ff00fe74a5
llli0v0 <li.gang.web@gmail.com>
2019-06-16 12:05:27 +0800

    重写1072:expressionless:

--- leetcode-solutions/hash-table/solution-1072/solution.js
+++ leetcode-solutions/hash-table/solution-1072/solution.js
@@ -5,29 +5,32 @@
 var maxEqualRowsAfterFlips = function(matrix) {
   let hash = {};
   for (let i = 0; i < matrix.length; i++) {
-    let k = helper(matrix[i]);
+    let a = [];
+    let b = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
+        a.push(j);
+      } else {
+        b.push(j);
+      }
+    }
+    let c;
+    if (a.length < b.length) {
+      c = a;
+    } else if (a.length > b.length) {
+      c = b;
+    } else {
+      if (a[0] < b[0]) {
+        c = a;
+      } else {
+        c = b;
+      }
+    }
+    let k = c.join();
     if (hash[k] === undefined) hash[k] = 0;
     hash[k] += 1;
   }
   let result = 0;
-  for (let v in hash) {
-    result = Math.max(result, hash[v]);
-  }
+  for (let k in hash) result = Math.max(result, hash[k]);
   return result;
-  function helper(l) {
-    let z = [];
-    let o = [];
-    for (let i = 0; i < l.length; i++) {
-      if (l[i] === 0) {
-        z.push(i);
-      } else {
-        o.push(i);
-      }
-    }
-    if (z.length === o.length) {
-      if (z[0] > o[0]) return o.join();
-      return z.join();
-    }
-    return z.length > o.length ? o.join() : z.join();
-  }
 };
