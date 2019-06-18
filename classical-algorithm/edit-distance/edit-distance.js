88a3fda20519857a751f41d92bcc030d9d4c43d5
llli0v0 <li.gang.web@gmail.com>
2019-06-18 09:47:05 +0800

    edit distance:open_mouth:

---
+++ classical-algorithm/edit-distance/edit-distance.js
@@ -0,0 +1,26 @@
+/**
+ *     h o r s e
+ *   0 1 2 3 4 5
+ * r 1 1 2 2 3 4
+ * o 2 2 1 2 3 4
+ * s 3 3 2 2 2 3
+ * 
+ * 编辑距离是经典的动态规划问题
+ * 常用来衡量两个字符串的相似度
+ */
+
+function EditDistance(A, B) {
+  let dp = new Array(A.length + 1).fill(null).map(() => new Array(B.length + 1).fill(0));
+  for (let i = 0; i < dp.length; i++) dp[i][0] = i;
+  for (let i = 0; i < dp[0].length; i++) dp[0][i] = i;
+  for (let i = 1; i < dp.length; i++) {
+    for (let j = 1; j < dp[0].length; j++) {
+      dp[i][j] = Math.min(
+        dp[i - 1][j] + 1,
+        dp[i][j - 1] + 1,
+        dp[i - 1][j - 1] + (A[i - 1] === B[j - 1] ? 0 : 1)
+      );
+    }
+  }
+  return dp[dp.length - 1][dp[0].length - 1];
+}
--- leetcode-solutions/dp/solution-0072/solution.js
+++ leetcode-solutions/dp/solution-0072/solution.js
@@ -3,25 +3,22 @@
  * @param {string} word2
  * @return {number}
  */
+/**
+ * 回顾一下 经典的编辑距离
+ * 优化了代码
+ */
 var minDistance = function(word1, word2) {
-  let matrix = new Array(word1.length + 1).fill(null).map(() => new Array(word2.length + 1).fill(null));
-  for (let i = 0; i <= word1.length; i++) {
-    matrix[i][0] = i;
-  }
-  for (let i = 0; i <= word2.length; i++) {
-    matrix[0][i] = i;
-  }
-  for (let i = 1; i <= word1.length; i++) {
-    for (let j = 1; j <= word2.length; j++) {
-      let word1Index = i - 1, word2Index = j - 1;
-      if (word1[word1Index] === word2[word2Index]) {
-        matrix[i][j] = matrix[i - 1][j - 1];
-      } else {
-        matrix[i][j] = Math.min(matrix[i - 1][j], matrix[i - 1][j - 1], matrix[i][j - 1]) + 1;
-      }
+  let dp = new Array(word1.length + 1).fill(null).map(() => new Array(word2.length + 1).fill(0));
+  for (let i = 0; i < dp.length; i++) dp[i][0] = i;
+  for (let i = 0; i < dp[0].length; i++) dp[0][i] = i;
+  for (let i = 1; i < dp.length; i++) {
+    for (let j = 1; j < dp[0].length; j++) {
+      dp[i][j] = Math.min(
+        dp[i - 1][j] + 1,
+        dp[i][j - 1] + 1,
+        dp[i - 1][j - 1] + (word1[i - 1] === word2[j - 1] ? 0 : 1)
+      );
     }
   }
-  return matrix[word1.length][word2.length];
+  return dp[dp.length - 1][dp[0].length - 1];
 };
-
-module.exports = { solution: minDistance };
