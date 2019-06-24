749912b3eace4df247ce5d315792edca4204ec4b
llli0v0 <li.gang.web@gmail.com>
2019-06-24 08:05:58 +0800

    :grimacing:solution 104

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-# 这是我的leetcode解答 :kissing_smiling_eyes:
-
-我正在回顾之前做过的题目. :sunglasses:
-
-我会尽力解决每道题目.
-
-正在进行中...... :whale:
-
-## 计划
-
-1. 近期leetcode进度放缓(维持每天两道中等 或 一道困难)，我打算近期对 classical-algorithm 和 data-structures 进行优化，包括 代码优化、编写test
-
-2. 对 leetcode-solutions 进行整理，主要针对目录内容单一化，leetcode-solutions 今后只记录我的 leetcode 答案
+:grimacing::grimacing::grimacing:
---
+++ leetcode-solutions/tree/solution-0104/solution.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  return helper(root);
+
+  function helper(node) {
+    if (!node) return 0;
+    return Math.max(helper(node.left), helper(node.right)) + 1;
+  }
+};
