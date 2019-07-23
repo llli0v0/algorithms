6e1140e273a7cf3ccfcce3b5f22e89d6c1b87b4c
llli0v0 <li.gang.web@gmail.com>
2019-07-23 12:55:09 +0800

    redo 72

--- classical-algorithm/edit-distance/edit-distance.js
+++ classical-algorithm/edit-distance/edit-distance.js
@@ -11,16 +11,15 @@
 
 function EditDistance(A, B) {
   let dp = new Array(A.length + 1).fill(null).map(() => new Array(B.length + 1).fill(0));
+
   for (let i = 0; i < dp.length; i++) dp[i][0] = i;
   for (let i = 0; i < dp[0].length; i++) dp[0][i] = i;
+
   for (let i = 1; i < dp.length; i++) {
     for (let j = 1; j < dp[0].length; j++) {
-      dp[i][j] = Math.min(
-        dp[i - 1][j] + 1,
-        dp[i][j - 1] + 1,
-        dp[i - 1][j - 1] + (A[i - 1] === B[j - 1] ? 0 : 1)
-      );
+      dp[i][j] = Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + (A[i - 1] === B[j - 1] ? 0 : 1));
     }
   }
+  
   return dp[dp.length - 1][dp[0].length - 1];
 }
