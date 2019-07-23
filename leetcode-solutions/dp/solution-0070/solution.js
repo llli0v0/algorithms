b46035a28d3c5bc6a5643dd52272b5e55e14f0da
llli0v0 <li.gang.web@gmail.com>
2019-07-23 12:48:48 +0800

    redo 70

--- leetcode-solutions/dp/solution-0070/solution.js
+++ leetcode-solutions/dp/solution-0070/solution.js
@@ -3,14 +3,17 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-  let dp = { 0: 1, 1: 1 };
+  let dp = { '-1': 0, 0: 1, 1: 1 };
+
   helper(n);
+
   return dp[n];
 
   function helper(n) {
-    if (n < 0) return 0;
-    if (dp[n]) return dp[n];
+    if (dp[n] !== undefined) return dp[n];
+
     dp[n] = helper(n - 2) + helper(n - 1);
+
     return dp[n];
   }
 };
