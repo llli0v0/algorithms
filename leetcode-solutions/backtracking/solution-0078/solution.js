367fd2144bb3168bfa31a5728db9a01c823ac08b
llli0v0 <li.gang.web@gmail.com>
2019-07-23 09:51:01 +0800

    redo 78

--- leetcode-solutions/backtracking/solution-0078/solution.js
+++ leetcode-solutions/backtracking/solution-0078/solution.js
@@ -3,19 +3,16 @@
  * @return {number[][]}
  */
 var subsets = function(nums) {
-  let re = new Set();
-  for (let i = 0; i < nums.length; i++) {
-    let d = '' + nums[i];
-    findSub(d, nums.slice(i + 1));
-    re.add(d);
-  }
-  return Array.from(re).map(i => i.split('_')).concat([[]]);
-  function findSub(s, ar) {
-    if (!ar.length) return;
-    for (let i = 0; i < ar.length; i++) {
-      let ss = s + '_' + ar[i];
-      findSub(ss, ar.slice(i + 1));
-      re.add(ss);
-    }
+  let result = [];
+
+  helper(nums, []);
+
+  return result;
+
+  function helper(nums, N) {
+    if (!nums.length) return result.push(N);
+
+    helper(nums.slice(1), N.concat([nums[0]]));
+    helper(nums.slice(1), N);
   }
 };
