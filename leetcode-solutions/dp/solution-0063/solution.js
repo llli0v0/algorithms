22390cd044c08b931d3327b5790f55d1ef9aa419
llli0v0 <li.gang.web@gmail.com>
2019-07-23 10:58:17 +0800

    redo 63

--- leetcode-solutions/dp/solution-0063/solution.js
+++ leetcode-solutions/dp/solution-0063/solution.js
@@ -3,37 +3,31 @@
  * @return {number}
  */
 var uniquePathsWithObstacles = function(obstacleGrid) {
-  for (let i = 0; i < obstacleGrid.length; i++) {
-    for (let j = 0; j < obstacleGrid[0].length; j++) {
-      if (obstacleGrid[i][j] === 1) obstacleGrid[i][j] = '*';
+  let dp = obstacleGrid;
+
+  for (let i = 0; i < dp.length; i++) {
+    for (let j = 0; j < dp[i].length; j++) {
+      if (dp[i][j]) dp[i][j] = -1;
     }
   }
-  if (obstacleGrid[obstacleGrid.length - 1][obstacleGrid[0].length - 1] === '*') return 0;
-  for (let i = 0; i < obstacleGrid.length; i++) {
-    if (obstacleGrid[i][0] === '*') break;
-    obstacleGrid[i][0] = 1;
+
+  dp[0][0] = dp[0][0] === -1 ? -1 : 1;
+
+  for (let i = 1; i < dp.length; i++) {
+    if (dp[i - 1][0] === 1 && dp[i][0] !== -1) dp[i][0] = 1;
   }
-  for (let i = 0; i < obstacleGrid[0].length; i++) {
-    if (obstacleGrid[0][i] === '*') break;
-    obstacleGrid[0][i] = 1;
+
+  for (let i = 1; i < dp[0].length; i++) {
+    if (dp[0][i - 1] === 1 && dp[0][i] !== -1) dp[0][i] = 1;
   }
-  for (let i = 1; i < obstacleGrid.length; i++) {
-    for (let j = 1; j < obstacleGrid[0].length; j++) {
-      if (obstacleGrid[i][j] === '*') continue;
-      if (obstacleGrid[i - 1][j] === '*' || obstacleGrid[i][j - 1] === '*') {
-        if (obstacleGrid[i - 1][j] === '*' && obstacleGrid[i][j - 1] === '*') {
-          obstacleGrid[i][j] = 0;
-        } else {
-          obstacleGrid[i][j] = obstacleGrid[i - 1][j] === '*' ? obstacleGrid[i][j - 1] : obstacleGrid[i - 1][j];
-        }
-      } else if (obstacleGrid[i - 1][j] === 0 || obstacleGrid[i][j - 1] === 0) {
-        obstacleGrid[i][j] = obstacleGrid[i - 1][j] === 0 ? obstacleGrid[i][j - 1] : obstacleGrid[i - 1][j];
-      } else {
-        obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+
+  for (let i = 1; i < dp.length; i++) {
+    for (let j = 1; j < dp[i].length; j++) {
+      if (dp[i][j] !== -1) {
+        dp[i][j] = (dp[i - 1][j] === -1 ? 0 : dp[i - 1][j]) + (dp[i][j - 1] === -1 ? 0 : dp[i][j - 1]);
       }
     }
   }
-  return obstacleGrid[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
-};
 
-console.log(uniquePathsWithObstacles([[0,1],[1,0]]));
+  return dp[dp.length - 1][dp[0].length - 1] === -1 ? 0 : dp[dp.length - 1][dp[0].length - 1];
+};
