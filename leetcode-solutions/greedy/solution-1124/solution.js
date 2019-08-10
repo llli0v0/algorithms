1f196220d3ec34776af8fdc69d07c09f3eaf1c93
llli0v0 <li.gang.web@gmail.com>
2019-08-10 23:42:28 +0800

    :grimacing:solution 1124

---
+++ leetcode-solutions/greedy/solution-1124/solution.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} hours
+ * @return {number}
+ */
+var longestWPI = function(hours) {
+  let H = [];
+  let result = 0;
+
+  for (let i = 0; i < hours.length; i++) {
+    if (hours[i] > 8) {
+      H.push(1);
+      result = 1;
+    } else {
+      H.push(-1);
+    }
+  }
+
+  let hash = {};
+  let S = 0;
+
+  for (let i = 0; i < H.length; i++) {
+    S += H[i];
+    if (hash[S] === undefined) hash[S] = i;
+
+    if (S >= 1) result = i + 1;
+
+    if (hash[S - 1] !== undefined) {
+      result = Math.max(result, i - hash[S - 1]);
+    }
+  }
+
+  return result;
+};
