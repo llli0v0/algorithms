909db052d5a1c2cb3699c396107b5a317fc83e3e
llli0v0 <li.gang.web@gmail.com>
2019-08-22 12:49:15 +0800

    redo 115

--- leetcode-solutions/dp/solution-0115/solution.js
+++ leetcode-solutions/dp/solution-0115/solution.js
@@ -4,22 +4,29 @@
  * @return {number}
  */
 var numDistinct = function(s, t) {
-  let matrix = new Array(t.length).fill(null).map(() => new Array(s.length).fill(0));
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === t[0]) matrix[0][i] = 1;
+  if (!s || !t) return 0;
+
+  let dp = new Array(t.length).fill(null).map(() => new Array(s.length).fill(0));
+
+  if (s[0] === t[0]) dp[0][0] = 1;
+
+  for (let i = 1; i < s.length; i++) {
+    if (s[i] === t[0]) {
+      dp[0][i] = dp[0][i - 1] + 1;
+    } else {
+      dp[0][i] = dp[0][i - 1];
+    }
   }
+
   for (let i = 1; i < t.length; i++) {
-    for (let j = 0; j < s.length; j++) {
-      if (j >= i && t[i] === s[j]) {
-        let result = 0;
-        for (let n = 0; n < j; n++) result += matrix[i - 1][n];
-        matrix[i][j] = result;
+    for (let j = 1; j < s.length; j++) {
+      if (t[i] === s[j]) {
+        dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
+      } else {
+        dp[i][j] = dp[i][j - 1];
       }
     }
   }
-  let result = 0;
-  for (let i = 0; i < s.length; i++) result += matrix[t.length - 1][i];
-  return result;
-};
 
-module.exports = { solution: numDistinct };
+  return dp[dp.length - 1][dp[0].length - 1];
+};
