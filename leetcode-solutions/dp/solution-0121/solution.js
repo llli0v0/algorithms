babd553cc8d69a7f7c734410f81678830272c69e
llli0v0 <li.gang.web@gmail.com>
2019-09-24 12:00:30 +0800

    redo 121

--- leetcode-solutions/dp/solution-0121/solution.js
+++ leetcode-solutions/dp/solution-0121/solution.js
@@ -3,11 +3,15 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-  if (!prices.length) return 0;
-  let currentMin = prices[0], subBest = { 0: 0 };
+  if (prices.length < 2) return 0;
+
+  let M = prices[0];
+  let result = 0;
+
   for (let i = 1; i < prices.length; i++) {
-    if (prices[i] < currentMin) currentMin = prices[i];
-    subBest[i] = Math.max(subBest[i - 1], prices[i] - currentMin);
+    result = Math.max(result, prices[i] - M);
+    M = Math.min(M, prices[i]);
   }
-  return subBest[prices.length - 1];
+
+  return result;
 };
