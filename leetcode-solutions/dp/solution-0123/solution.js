9952109a857f97986cb121324420d045a1b8b596
llli0v0 <li.gang.web@gmail.com>
2019-09-24 15:09:49 +0800

    redo 123

--- leetcode-solutions/dp/solution-0123/solution.js
+++ leetcode-solutions/dp/solution-0123/solution.js
@@ -3,29 +3,25 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-  let currentMin = prices[0], subs = [], subBest = {}, reverseSubBest = {}, currentMax, result = 0;
+  let dp = [0];
+  let m1 = prices[0];
+  let result = 0;
+
   for (let i = 1; i < prices.length; i++) {
-    if (prices[i] < prices[i - 1]) {
-      subs.push([currentMin, prices[i - 1]]);
-      currentMin = prices[i];
+    dp.push(Math.max(dp[i - 1] === undefined ? 0 : dp[i - 1], prices[i] - m1));
+    
+    m1 = Math.min(m1, prices[i]);
+
+    let m2 = prices[i - 1];
+
+    for (let j = i - 1; j >= 0; j--) {
+      m2 = Math.min(m2, prices[j]);
+
+      let a = Math.max(0, prices[i] - m2);
+
+      result = Math.max(result, a + (dp[j - 1] === undefined ? 0 : dp[j - 1]));
     }
   }
-  if (prices[prices.length - 1] > currentMin) subs.push([currentMin, prices[prices.length - 1]]);
-  if (!subs.length) return 0;
-  currentMin = subs[0][0];
-  subBest['0-0'] = subs[0][1] - subs[0][0];
-  for (let i = 1; i < subs.length; i++) {
-    if (subs[i][0] < currentMin) currentMin = subs[i][0];
-    subBest['0-' + i] = Math.max(subBest['0-' + (i - 1)], subs[i][1] - currentMin);
-  }
-  currentMax = subs[subs.length - 1][1];
-  reverseSubBest[subs.length - 1 + '-' + (subs.length - 1)] = subs[subs.length - 1][1] - subs[subs.length - 1][0];
-  for (let i = subs.length - 2; i >= 0; i--) {
-    if (subs[i][1] > currentMax) currentMax = subs[i][1];
-    reverseSubBest[i + '-' + (subs.length - 1)] = Math.max(reverseSubBest[i + 1 + '-' + (subs.length - 1)], currentMax - subs[i][0]);
-  }
-  for (let i = 0; i < subs.length - 1; i++) {
-    result = Math.max(result, subBest['0-' + i] + reverseSubBest[i + 1 + '-' + (subs.length - 1)]);
-  }
-  return Math.max(result, subBest['0-' + (subs.length - 1)]);
+
+  return result;
 };
