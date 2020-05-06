73a61db16cf6e90f188ff8a59a5a565ab5a3aec4
llli0v0 <li.gang.web@gmail.com>
2020-05-06 17:17:03 +0800

    :grimacing:solution 1439

---
+++ leetcode-solutions/heap/solution-1439/solution.js
@@ -0,0 +1,130 @@
+/**
+ * @param {number[][]} mat
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(mat, k) {
+  let heap = new Heap();
+  for (let i = 0; i < mat[0].length; i++) {
+    heap.heappush(-mat[0][i]);
+    if (heap.heap.length > k) heap.heappop();
+  }
+  for (let i = 1; i < mat.length; i++) {
+    let heap2 = new Heap();
+    for (let j = 0; j < mat[i].length; j++) {
+      for (let n = 0; n < heap.heap.length; n++) {
+        heap2.heappush(-(-heap.heap[n] + mat[i][j]));
+        if (heap2.heap.length > k) heap2.heappop();
+      }
+    }
+    heap = heap2;
+  }
+  return -heap.heap[0];
+};
+
+class Heap {
+  /**
+   * @param {String} key
+   */
+  constructor(key) {
+    this.heap = [];
+    this.isMaxHeap = false;
+    this.compareKey = key;
+
+    if (key !== undefined) {
+      this.comparetor = (a, b) => a[key] - b[key];
+    } else {
+      this.comparetor = (a, b) => a - b;
+    }
+  }
+
+  lessThan(a, b) {
+    return this.comparetor(a, b) < 0;
+  }
+
+  equal(a, b) {
+    return this.comparetor(a, b) === 0;
+  }
+
+  biggerThan(a, b) {
+    return this.comparetor(a, b) > 0;
+  }
+
+  heappush(item) {
+    if (this.isMaxHeap) {
+      if (this.compareKey !== undefined) {
+        item[this.compareKey] = -item[this.compareKey];
+      } else {
+        item = -item;
+      }
+    }
+
+    this.heap.push(item);
+
+    let index = this.heap.length - 1;
+    let parentIndex = this.getParentIndex(index);
+
+    while (parentIndex >= 0 && this.biggerThan(this.heap[parentIndex], this.heap[index])) {
+      let temp = this.heap[parentIndex];
+
+      this.heap[parentIndex] = this.heap[index];
+      this.heap[index] = temp;
+      index = parentIndex;
+      parentIndex = this.getParentIndex(index);
+    }
+  }
+
+  heappop() {
+    if (this.heap.length <= 1) return this.heap.pop();
+
+    let popItem = this.heap[0];
+    this.heap[0] = this.heap.pop();
+
+    let index = 0;
+    let leftChildIndex = this.getLeftChildIndex(index);
+    let rightChildIndex = this.getRightChildIndex(index);
+
+    while (
+      leftChildIndex < this.heap.length && this.biggerThan(this.heap[index], this.heap[leftChildIndex]) ||
+      rightChildIndex < this.heap.length && this.biggerThan(this.heap[index], this.heap[rightChildIndex])
+    ) {
+      let swapIndex;
+
+      if (leftChildIndex < this.heap.length && rightChildIndex < this.heap.length) {
+        swapIndex = this.lessThan(this.heap[leftChildIndex], this.heap[rightChildIndex]) ? leftChildIndex : rightChildIndex;
+      } else {
+        swapIndex = leftChildIndex;
+      }
+
+      let temp = this.heap[index];
+
+      this.heap[index] = this.heap[swapIndex];
+      this.heap[swapIndex] = temp;
+      index = swapIndex;
+      leftChildIndex = this.getLeftChildIndex(index);
+      rightChildIndex = this.getRightChildIndex(index);
+    }
+
+    if (this.isMaxHeap) {
+      if (this.compareKey !== undefined) {
+        popItem[this.compareKey] = -popItem[this.compareKey];
+      } else {
+        popItem = -popItem;
+      }
+    }
+
+    return popItem;
+  }
+
+  getParentIndex(childIndex) {
+    return Math.floor((childIndex - 1) / 2);
+  }
+
+  getLeftChildIndex(parentIndex) {
+    return parentIndex * 2 + 1;
+  }
+
+  getRightChildIndex(parentIndex) {
+    return parentIndex * 2 + 2;
+  }
+}
