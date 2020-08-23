0437ff35989a8ae888635703f9351b1aef48fe2c
ligang1 <ligang1@vipkid.com.cn>
2020-08-23 22:38:11 +0800

    Stone Game V pre sum

--- leetcode-solutions/dp/Stone Game V/solution.js
+++ leetcode-solutions/dp/Stone Game V/solution.js
@@ -4,7 +4,8 @@
  */
 var stoneGameV = function(stoneValue) {
   let computed = {};
-  let sumComputed = {};
+  let sumComputed = { '-1': 0 };
+  for (let i = 0; i < stoneValue.length; i++) sumComputed[i] = sumComputed[i - 1] + stoneValue[i];
   return dp(0, stoneValue.length - 1);
 
   function dp(l, r) {
@@ -13,22 +14,8 @@ var stoneGameV = function(stoneValue) {
     if (computed[key] !== undefined) return computed[key];
     computed[key] = -Infinity;
     for (let i = l; i < r; i++) {
-      let k1 = l + '-' + i;
-      let v1 = 0;
-      if (sumComputed[k1] !== undefined) {
-        v1 = sumComputed[k1];
-      } else {
-        for (let j = l; j <= i; j++) v1 += stoneValue[j];
-        sumComputed[k1] = v1;
-      }
-      let k2 = i + 1 + '-' + r;
-      let v2 = 0;
-      if (sumComputed[k2] !== undefined) {
-        v2 = sumComputed[k2];
-      } else {
-        for (let j = i + 1; j <= r; j++) v2 += stoneValue[j];
-        sumComputed[k2] = v2;
-      }
+      let v1 = sumComputed[i] - sumComputed[l - 1];
+      let v2 = sumComputed[r] - sumComputed[i];
       if (v1 === v2) {
         computed[key] = Math.max(computed[key], dp(l, i) + v1, dp(i + 1, r) + v2);
       } else {
