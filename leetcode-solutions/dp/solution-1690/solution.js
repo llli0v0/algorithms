79bc4628746a0182529a154fbd227b7eff46c061
ligang1 <ligang1@vipkid.com.cn>
2020-12-15 15:29:26 +0800

    1690

---
+++ leetcode-solutions/dp/solution-1690/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var stoneGameVII = function(stones) {
+  let computed = {};
+  return dp(0, stones.length - 1, 1, stones.reduce((a, b) => a + b));
+
+  function dp(l, r, p, sum) {
+    if (l >= r) return 0;
+    let key = `${l} ${r}`;
+    if (computed[key] !== undefined) return computed[key];
+    if (p) {
+      computed[key] = Math.max(dp(l, r - 1, 0, sum - stones[r]) + sum - stones[r], dp(l + 1, r, 0, sum - stones[l]) + sum - stones[l])
+    } else {
+      computed[key] = Math.min(dp(l, r - 1, 1, sum - stones[r]) - (sum - stones[r]), dp(l + 1, r, 1, sum - stones[l]) - (sum - stones[l]));
+    }
+    return computed[key];
+  }
+};
