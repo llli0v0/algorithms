bc58cf89d780cf74b22dc86ee10f11b41ef39b4d
ligang1 <llli0vv0@gmail.com>
2021-05-10 13:46:34 +0800

    😬segment tree and solution 1521

--- leetcode-solutions/segment-tree/solution-1521/solution.js
+++ leetcode-solutions/segment-tree/solution-1521/solution.js
@@ -1,3 +1,26 @@
+/**
+ * @param {number[]} arr
+ * @param {number} target
+ * @return {number}
+ */
+ var closestToTarget = function(arr, target) {
+  let tree = new SegmentTree(arr);
+  let result = Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    let l = i;
+    let r = arr.length - 1;
+    while (l < r) {
+      let m = Math.floor((l + r) / 2);
+      let val = tree.getVal(i, m);
+      if (val > target) l = m + 1;
+      else r = m;
+      result = Math.min(result, Math.abs(val - target));
+    }
+    result = Math.min(result, Math.abs(tree.getVal(i, l) - target));
+  }
+  return result;
+};
+
 class Node {
   constructor() {
     this.val = 0;
@@ -37,27 +60,27 @@ class SegmentTree {
       this.root.r = r;
       this.root.left = this.init(l, m);
       this.root.right = this.init(m + 1, r);
-      this.root.val = this.root.left.val + this.root.right.val;
+      this.root.val = this.root.left.val & this.root.right.val;
     } else {
       let node = new Node();
       node.l = l;
       node.r = r;
       node.left = this.init(l, m);
       node.right = this.init(m + 1, r);
-      node.val = node.left.val + node.right.val;
+      node.val = node.left.val & node.right.val;
       return node;
     }
   }
 
   getVal(l, r, node = this.root) {
     if (l === node.l && r === node.r) return node.val;
-    if (l > node.r || r < node.l) return 0;
+    if (l > node.r || r < node.l) return (1 << 30) - 1;
     if (node.right && l >= node.right.l) {
       return this.getVal(l, r, node.right);
     } else if (node.left && r <= node.left.r) {
       return this.getVal(l, r, node.left);
     } else {
-      return this.getVal(l, node.left.r, node.left) + this.getVal(node.right.l, r, node.right);
+      return this.getVal(l, node.left.r, node.left) & this.getVal(node.right.l, r, node.right);
     }
   }
 }
