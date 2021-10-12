6a837cecc8dfdf4d16697a3287e2d47dd9c81734
ligang1 <llli0vv0@gmail.com>
2021-10-12 23:14:39 +0800

    solution 2034

---
+++ leetcode-solutions/heap/solution-2034/solution.js
@@ -0,0 +1,166 @@
+var StockPrice = function() {
+  this.minHeap = new Heap(0);
+  this.maxHeap = new Heap(0);
+  this.maxHeap.isMaxHeap = true;
+  this.priceMap = {};
+  this.near = 0;
+};
+
+/** 
+ * @param {number} timestamp 
+ * @param {number} price
+ * @return {void}
+ */
+StockPrice.prototype.update = function(timestamp, price) {
+  this.priceMap[timestamp] = price;
+  this.minHeap.heappush([price, timestamp]);
+  this.maxHeap.heappush([price, timestamp]);
+  this.near = Math.max(this.near, timestamp);
+};
+
+/**
+ * @return {number}
+ */
+StockPrice.prototype.current = function() {
+  return this.priceMap[this.near];
+};
+
+/**
+ * @return {number}
+ */
+StockPrice.prototype.maximum = function() {
+  let heap = this.maxHeap.heap;
+  while (this.priceMap[heap[0][1]] !== -heap[0][0]) this.maxHeap.heappop();
+  return -heap[0][0];
+};
+
+/**
+ * @return {number}
+ */
+StockPrice.prototype.minimum = function() {
+  let heap = this.minHeap.heap;
+  while (this.priceMap[heap[0][1]] !== heap[0][0]) this.minHeap.heappop();
+  return heap[0][0];
+};
+
+/**
+ * Your StockPrice object will be instantiated and called as such:
+ * var obj = new StockPrice()
+ * obj.update(timestamp,price)
+ * var param_2 = obj.current()
+ * var param_3 = obj.maximum()
+ * var param_4 = obj.minimum()
+ */
+
+ class Heap {
+  /**
+   * @param {String} key
+   */
+  constructor(key) {
+    this.heap = [];
+    this.isMaxHeap = false;
+    this.compareKey = key;
+
+    if (key !== undefined) {
+      this.comparetor = (a, b) => a[key] - b[key];
+    } else {
+      this.comparetor = (a, b) => a - b;
+    }
+  }
+
+  lessThan(a, b) {
+    return this.comparetor(a, b) < 0;
+  }
+
+  equal(a, b) {
+    return this.comparetor(a, b) === 0;
+  }
+
+  biggerThan(a, b) {
+    return this.comparetor(a, b) > 0;
+  }
+
+  heappush(item) {
+    if (this.isMaxHeap) {
+      if (this.compareKey !== undefined) {
+        item[this.compareKey] = -item[this.compareKey];
+      } else {
+        item = -item;
+      }
+    }
+
+    this.heap.push(item);
+
+    let index = this.heap.length - 1;
+    let parentIndex = this.getParentIndex(index);
+
+    while (parentIndex >= 0 && this.biggerThan(this.heap[parentIndex], this.heap[index])) {
+      let temp = this.heap[parentIndex];
+
+      this.heap[parentIndex] = this.heap[index];
+      this.heap[index] = temp;
+      index = parentIndex;
+      parentIndex = this.getParentIndex(index);
+    }
+  }
+
+  heappop() {
+    if (this.heap.length <= 1) {
+      let popItem = this.heap.pop();
+      if (this.isMaxHeap) {
+        return -popItem;
+      }
+      return popItem;
+    }
+
+    let popItem = this.heap[0];
+    this.heap[0] = this.heap.pop();
+
+    let index = 0;
+    let leftChildIndex = this.getLeftChildIndex(index);
+    let rightChildIndex = this.getRightChildIndex(index);
+
+    while (
+      leftChildIndex < this.heap.length && this.biggerThan(this.heap[index], this.heap[leftChildIndex]) ||
+      rightChildIndex < this.heap.length && this.biggerThan(this.heap[index], this.heap[rightChildIndex])
+    ) {
+      let swapIndex;
+
+      if (leftChildIndex < this.heap.length && rightChildIndex < this.heap.length) {
+        swapIndex = this.lessThan(this.heap[leftChildIndex], this.heap[rightChildIndex]) ? leftChildIndex : rightChildIndex;
+      } else {
+        swapIndex = leftChildIndex;
+      }
+
+      let temp = this.heap[index];
+
+      this.heap[index] = this.heap[swapIndex];
+      this.heap[swapIndex] = temp;
+      index = swapIndex;
+      leftChildIndex = this.getLeftChildIndex(index);
+      rightChildIndex = this.getRightChildIndex(index);
+    }
+
+    if (this.isMaxHeap) {
+      if (this.compareKey !== undefined) {
+        popItem[this.compareKey] = -popItem[this.compareKey];
+      } else {
+        popItem = -popItem;
+      }
+    }
+
+    return popItem;
+  }
+
+  getParentIndex(childIndex) {
+    return Math.floor((childIndex - 1) / 2);
+  }
+
+  getLeftChildIndex(parentIndex) {
+    return parentIndex * 2 + 1;
+  }
+
+  getRightChildIndex(parentIndex) {
+    return parentIndex * 2 + 2;
+  }
+}
