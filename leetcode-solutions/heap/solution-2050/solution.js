7edc75548b5481bc7d4c97a09568f759a4b638f1
ligang1 <llli0vv0@gmail.com>
2021-10-31 16:44:14 +0800

    solution 2050

---
+++ leetcode-solutions/heap/solution-2050/solution.js
@@ -0,0 +1,152 @@
+/**
+ * @param {number} n
+ * @param {number[][]} relations
+ * @param {number[]} time
+ * @return {number}
+ */
+var minimumTime = function(n, relations, time) {
+  let depCountMap = {};
+  let relatMap = {};
+  let start = new Set(new Array(n).fill(null).map((i, index) => index + 1));
+  for (let i = 0; i < relations.length; i++) {
+    let [dep, main] = relations[i];
+    if (depCountMap[main] === undefined) depCountMap[main] = 0;
+    depCountMap[main] += 1;
+    start.delete(main);
+    if (relatMap[dep] === undefined) relatMap[dep] = [];
+    relatMap[dep].push(main);
+  }
+  let heap = new Heap(0);
+  start = Array.from(start);
+  for (let i = 0; i < start.length; i++) {
+    heap.heappush([time[start[i] - 1], start[i]]);
+  }
+  let curTime = 0;
+  while (heap.heap.length) {
+    let [t, sign] = heap.heappop();
+    curTime = t;
+    if (relatMap[sign]) {
+      let arr = relatMap[sign];
+      for (let i = 0; i < arr.length; i++) {
+        depCountMap[arr[i]] -= 1;
+        if (depCountMap[arr[i]] === 0) {
+          heap.heappush([curTime + time[arr[i] - 1], arr[i]]);
+        }
+      }
+    }
+  }
+  return curTime;
+};
+
+class Heap {
+  /**
+   * @param {String} key
+   */
+  constructor(key) {
+    this.heap = [];
+    this.isMaxHeap = false;
+    this.compareKey = key;
+
+    if (key !== undefined) {
+      this.comparetor = (a, b) => a[key] - b[key];
+    } else {
+      this.comparetor = (a, b) => a - b;
+    }
+  }
+
+  lessThan(a, b) {
+    return this.comparetor(a, b) < 0;
+  }
+
+  equal(a, b) {
+    return this.comparetor(a, b) === 0;
+  }
+
+  biggerThan(a, b) {
+    return this.comparetor(a, b) > 0;
+  }
+
+  heappush(item) {
+    if (this.isMaxHeap) {
+      if (this.compareKey !== undefined) {
+        item[this.compareKey] = -item[this.compareKey];
+      } else {
+        item = -item;
+      }
+    }
+
+    this.heap.push(item);
+
+    let index = this.heap.length - 1;
+    let parentIndex = this.getParentIndex(index);
+
+    while (parentIndex >= 0 && this.biggerThan(this.heap[parentIndex], this.heap[index])) {
+      let temp = this.heap[parentIndex];
+
+      this.heap[parentIndex] = this.heap[index];
+      this.heap[index] = temp;
+      index = parentIndex;
+      parentIndex = this.getParentIndex(index);
+    }
+  }
+
+  heappop() {
+    if (this.heap.length <= 1) {
+      let popItem = this.heap.pop();
+      if (this.isMaxHeap) {
+        return -popItem;
+      }
+      return popItem;
+    }
+
+    let popItem = this.heap[0];
+    this.heap[0] = this.heap.pop();
+
+    let index = 0;
+    let leftChildIndex = this.getLeftChildIndex(index);
+    let rightChildIndex = this.getRightChildIndex(index);
+
+    while (
+      leftChildIndex < this.heap.length && this.biggerThan(this.heap[index], this.heap[leftChildIndex]) ||
+      rightChildIndex < this.heap.length && this.biggerThan(this.heap[index], this.heap[rightChildIndex])
+    ) {
+      let swapIndex;
+
+      if (leftChildIndex < this.heap.length && rightChildIndex < this.heap.length) {
+        swapIndex = this.lessThan(this.heap[leftChildIndex], this.heap[rightChildIndex]) ? leftChildIndex : rightChildIndex;
+      } else {
+        swapIndex = leftChildIndex;
+      }
+
+      let temp = this.heap[index];
+
+      this.heap[index] = this.heap[swapIndex];
+      this.heap[swapIndex] = temp;
+      index = swapIndex;
+      leftChildIndex = this.getLeftChildIndex(index);
+      rightChildIndex = this.getRightChildIndex(index);
+    }
+
+    if (this.isMaxHeap) {
+      if (this.compareKey !== undefined) {
+        popItem[this.compareKey] = -popItem[this.compareKey];
+      } else {
+        popItem = -popItem;
+      }
+    }
+
+    return popItem;
+  }
+
+  getParentIndex(childIndex) {
+    return Math.floor((childIndex - 1) / 2);
+  }
+
+  getLeftChildIndex(parentIndex) {
+    return parentIndex * 2 + 1;
+  }
+
+  getRightChildIndex(parentIndex) {
+    return parentIndex * 2 + 2;
+  }
+}
