df4a9cee80e58638a1648f41e856c460ec8df5b6
ligang1 <llli0vv0@gmail.com>
2022-08-02 14:35:05 +0800

    solution 1374

---
+++ leetcode-solutions/string/solution-1374/solution.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var generateTheString = function(n) {
+  if (n % 2) {
+    return new Array(n).fill('a').join('');
+  }
+  return 'a' + new Array(n - 1).fill('b').join('');
+};
