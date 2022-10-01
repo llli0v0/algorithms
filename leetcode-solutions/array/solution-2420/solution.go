2dde174f459bb8775cfdb08b474665bfbbe915fe
ligang <llli0vv0@gmail.com>
2022-10-02 00:15:28 +0800

    solution 2420

---
+++ leetcode-solutions/array/solution-2420/solution.go
@@ -0,0 +1,36 @@
+package solution
+
+func goodIndices(nums []int, k int) (res []int) {
+	ilen, dlen := 1, 1
+	imap, dmap := make(map[int]bool), make(map[int]bool)
+	if k == 1 {
+		imap[0] = true
+		dmap[0] = true
+	}
+	for i := 1; i < len(nums); i++ {
+		v := nums[i]
+		pre := nums[i-1]
+		if v < pre {
+			ilen++
+			dlen = 1
+		} else if v > pre {
+			dlen++
+			ilen = 1
+		} else {
+			ilen++
+			dlen++
+		}
+		if ilen >= k {
+			imap[i] = true
+		}
+		if dlen >= k {
+			dmap[i-k+1] = true
+		}
+	}
+	for i := k; i < len(nums)-k; i++ {
+		if imap[i-1] && dmap[i+1] {
+			res = append(res, i)
+		}
+	}
+	return
+}
