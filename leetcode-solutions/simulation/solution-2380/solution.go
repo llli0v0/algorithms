dbd316190297632581b69e63f28c0f31ee699579
ligang <llli0vv0@gmail.com>
2022-10-02 19:09:42 +0800

    solution 2380

---
+++ leetcode-solutions/simulation/solution-2380/solution.go
@@ -0,0 +1,21 @@
+package solution
+
+func secondsToRemoveOccurrences(s string) (res int) {
+	runes := []rune(s)
+	next := true
+	for next {
+		f := false
+		for i := 0; i < len(runes); i++ {
+			if string(runes[i]) == "0" && i+1 < len(runes) && string(runes[i+1]) == "1" {
+				runes[i], runes[i+1] = runes[i+1], runes[i]
+				f = true
+				i++
+			}
+		}
+		next = f
+		if f {
+			res++
+		}
+	}
+	return
+}
