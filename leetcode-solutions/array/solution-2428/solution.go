07f9db3f625dc4d5f137e24fbb1670b0dcf2234b
ligang <llli0vv0@gmail.com>
2022-10-03 12:36:52 +0800

    solution 2428

---
+++ leetcode-solutions/array/solution-2428/solution.go
@@ -0,0 +1,22 @@
+package solution2428
+
+import "math"
+
+func maxSum(grid [][]int) (res int) {
+    for i, v := range grid {
+        for j, _ := range v {
+            if i >= 2 && j >= 2 {
+                var val int
+                for m := i; m > i-3; m-- {
+                    for n := j; n > j-3; n-- {
+                        val += grid[m][n]
+                    }
+                }
+                val -= grid[i-1][j-2]
+                val -= grid[i-1][j]
+                res = int(math.Max(float64(res), float64(val)))
+            }
+        }
+    }
+    return
+}
