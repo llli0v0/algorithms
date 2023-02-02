09028ecbb1c1b4fe20a934aa3f002560a1513991
ligang <llli0vv0@gmail.com>
2023-02-02 22:06:12 +0800

    solution 2551

---
+++ leetcode-solutions/greedy/solution-2551/solution.js
@@ -0,0 +1,89 @@
+/**
+ * @param {number[]} weights
+ * @param {number} k
+ * @return {number}
+ */
+var putMarbles = function(weights, k) {
+  if (k === weights.length) return 0;
+  let max = min = weights[0] + weights[weights.length - 1];
+  let maxHeap = new Heap((a, b) => b - a);
+  let minHeap = new Heap();
+  for (let i = 0; i < weights.length - 1; i++) {
+    let val = weights[i] + weights[i + 1];
+    maxHeap.push(val);
+    minHeap.push(val);
+  }
+  while (k - 1) {
+    max += maxHeap.pop();
+    min += minHeap.pop();
+    k--;
+  }
+  return max - min;
+};
+
+class Heap {
+  constructor(comparetor) {
+    this.list = [];
+    this.comparetor = comparetor ?? ((a, b) => a - b);
+  }
+
+  lessThan(a, b) {
+    return this.comparetor(a, b) < 0;
+  }
+
+  equal(a, b) {
+    return this.comparetor(a, b) === 0;
+  }
+
+  biggerThan(a, b) {
+    return this.comparetor(a, b) > 0;
+  }
+
+  push(item) {
+    let { list, getParentIndex } = this;
+    list.push(item);
+    let index = list.length - 1;
+    let pIndex = getParentIndex(index);
+    while (pIndex >= 0 && this.biggerThan(list[pIndex], list[index])) {
+      [list[index], list[pIndex]] = [list[pIndex], list[index]];
+      index = pIndex;
+      pIndex = getParentIndex(index);
+    }
+  }
+
+  pop() {
+    let { list, getLeftChildIndex, getRightChildIndex } = this;
+    if (list.length <= 1) return list.pop();
+    let popItem = list[0];
+    list[0] = list.pop();
+    let index = 0;
+    let [leftChildIndex, rightChildIndex] = [getLeftChildIndex(index), getRightChildIndex(index)];
+    while (
+      leftChildIndex < list.length && this.biggerThan(list[index], list[leftChildIndex]) ||
+      rightChildIndex < list.length && this.biggerThan(list[index], list[rightChildIndex])
+    ) {
+      let swapIndex;
+      if (leftChildIndex < list.length && rightChildIndex < list.length) {
+        swapIndex = this.lessThan(list[leftChildIndex], list[rightChildIndex]) ? leftChildIndex : rightChildIndex;
+      } else {
+        swapIndex = leftChildIndex;
+      }
+      [list[index], list[swapIndex]] = [list[swapIndex], list[index]];
+      index = swapIndex;
+      [leftChildIndex, rightChildIndex] = [getLeftChildIndex(index), getRightChildIndex(index)]
+    }
+    return popItem;
+  }
+
+  getParentIndex(childIndex) {
+    return Math.floor((childIndex - 1) / 2);
+  }
+
+  getLeftChildIndex(parentIndex) {
+    return parentIndex * 2 + 1;
+  }
+
+  getRightChildIndex(parentIndex) {
+    return parentIndex * 2 + 2;
+  }
+}
