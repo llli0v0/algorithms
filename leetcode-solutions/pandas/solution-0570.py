3c7bab536da797ac74b46dddd007f551880b59d8
llli0v0 <llli0vv0@gmail.com>
2023-08-25 22:47:44 +0800

    solution 570

---
+++ leetcode-solutions/pandas/solution-0570.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
+    df = employee.groupby('managerId').size().reset_index(name='count')
+    df = df[df['count'] >= 5]
+    df = pd.merge(df, employee, left_on='managerId', right_on='id', how='inner')
+    return df[['name']]
