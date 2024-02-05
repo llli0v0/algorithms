027f6290dbb33b8e0423ab42ee3adc4e9f65acb4
llli0v0 <llli0vv0@gmail.com>
2024-02-05 21:13:46 +0800

    solution 3031 and z-function😋

---
+++ classical-algorithm/string/z.js
@@ -0,0 +1,27 @@
+function Z(s) {
+  let z = new Array(s.length).fill(0);
+  let l = 0;
+  let r = 0;
+  for (let i = 1; i < s.length; i++) {
+    if (i > r) {
+      l = r = i;
+      while (r < s.length && s[r] === s[r - l]) {
+        r++;
+      }
+      z[i] = r - l;
+      r--;
+    } else {
+      if (r - i > z[i - l]) {
+        z[i] = z[i - l];
+      } else {
+        l = i;
+        while (r < s.length && s[r] === s[r - l]) {
+          r++;
+        }
+        z[i] = r - l;
+        r--;
+      }
+    }
+  }
+  return z;
+}
---
+++ leetcode-solutions/string/solution-3031/solution.ts
@@ -0,0 +1,28 @@
+function minimumTimeToInitialState(word: string, k: number): number {
+  let z: number[] = new Array(word.length).fill(0);
+  let l = 0;
+  let r = 0;
+  for (let i = 1; i < word.length; i++) {
+    if (i > r) {
+      l = r = i;
+      while (r < word.length && word[r] === word[r - l]) {
+        r++;
+      }
+      z[i] = r - l;
+      r--;
+    } else {
+      if (r - i > z[i - l]) {
+        z[i] = z[i - l];
+      } else {
+        l = i;
+        while (r < word.length && word[r] === word[r - l]) {
+          r++;
+        }
+        z[i] = r - l;
+        r--;
+      }
+    }
+    if (z[i] === word.length - i && i % k === 0) return i / k;
+  }
+  return Math.ceil(word.length / k);
+};
